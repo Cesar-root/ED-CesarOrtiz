8695cda39174fa937dd845285b78ea2f4a77fcc6
Cesar-root <15agostogalvan15@gmail.com>
2025-09-03 22:44:09 -0500

    Trabajos de estructura de control

--- Unidad-1/Fundamentos_de_programacion/Proyecto_Numero_Aleatorio/Program.cs
+++ Unidad-1/Fundamentos_de_programacion/Proyecto_Numero_Aleatorio/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.Design;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random random = new Random();
+        
+        int intNumeroUsuario = 0;
+        
+        int NumeroAdivinar = random.Next(0, 100);
+        
+        while (intNumeroUsuario != NumeroAdivinar) 
+        {
+            Console.WriteLine("Elige un numero entre 0 y 100");
+            intNumeroUsuario = int.Parse(Console.ReadLine());
+            if (intNumeroUsuario<NumeroAdivinar) 
+            {
+                Console.WriteLine("Un poco mas arriba");
+            }
+            else if (intNumeroUsuario > NumeroAdivinar)
+            {
+                Console.WriteLine("Un poco mas abajo");
+            }
+            
+        }
+        Console.WriteLine("ERES UN ADIVINO");
+    
+        }
+}
--- Unidad-1/Fundamentos_de_programacion/Proyecto_Numero_Primo/Program.cs
+++ Unidad-1/Fundamentos_de_programacion/Proyecto_Numero_Primo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Elige un numero");
+        int Numero = 0;
+        Numero = int.Parse(Console.ReadLine());
+        if (Numero % 2 == 0)
+        {
+            Console.WriteLine("No es primo");
+        }
+        else 
+        { 
+            Console.WriteLine("Es primo");
+        }
+    }
+}
