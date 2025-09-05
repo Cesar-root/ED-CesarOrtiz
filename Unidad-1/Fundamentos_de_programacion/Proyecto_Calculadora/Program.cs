4d454d3cab28780ebc579a59aae97276d56de39e
Cesar-root <15agostogalvan15@gmail.com>
2025-09-04 21:59:35 -0500

    Ejercicios de Funciones y Modulizacion

--- Unidad-1/Fundamentos_de_programacion/Proyecto_Calculadora/Program.cs
+++ Unidad-1/Fundamentos_de_programacion/Proyecto_Calculadora/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ingresa el primer numero de la operacion");
+        double dblN1 = 0;
+        dblN1 = double.Parse(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("Ingresa la operacion que deseas elaborar");
+        Console.WriteLine("");
+        Console.WriteLine(" + para suma");
+        Console.WriteLine(" - para resta");
+        Console.WriteLine("/ para division");
+        Console.WriteLine("* para multiplicar");
+        char chrOperacion;
+        chrOperacion = char.Parse(Console.ReadLine());
+        Console.Clear();
+        switch (chrOperacion)
+        {
+            case '+': Console.WriteLine("Ingresa el numero que le sumaras "); break;
+            case '-': Console.WriteLine("Ingresa el numero que le restaras"); break;
+            case '/': Console.WriteLine("Ingresa el numero por el que lo dividiras"); break;
+            case '*': Console.WriteLine("Ingresa el numero por el que lo multiplicaras"); break;
+            default: Console.WriteLine("No ingresaste lo solicitado"); break;
+        }
+        double dblN2 = 0;
+        dblN2 = double.Parse(Console.ReadLine());
+        Console.Clear();
+        Console.WriteLine("Tu resultado es "+Operacion(dblN1,dblN2,chrOperacion));
+    }
+        
+        static double Operacion(double N1, double N2, char Operacion)
+            {
+                switch(Operacion) 
+                {
+                    case '+': return N1 + N2;   ; break;
+                    case '-': return N1 - N2; break;
+                    case '/': return N1 / N2; break;
+                    case '*': return N1 * N2;
+                }
+                return 0;
+
+            }
+
+}
+
--- Unidad-1/Fundamentos_de_programacion/Proyecto_Promedio_Lista/Program.cs
+++ Unidad-1/Fundamentos_de_programacion/Proyecto_Promedio_Lista/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+internal class Program
+{
+    
+    private static void Main(string[] args)
+    {
+        static double Promedio(List<double> listanumeros)
+        {
+            if (listanumeros == null) 
+            {
+                return 0;
+            }
+            double suma = 0;
+            foreach (double num in listanumeros) 
+            {
+                 suma += num;
+            }
+            return suma/listanumeros.Count;
+        }
+
+
+        double dblNumero;
+        List<double> listaNumeros = new List<double>();
+        bool strClave = true;
+        while (true) 
+        {
+            string respuesta = "";
+            Console.WriteLine("Ingresa el numero que deseas guardar");
+            dblNumero = double.Parse(Console.ReadLine());
+            Console.WriteLine("¿Quieres terminar de agregar numeros? Si/No");
+            respuesta = Console.ReadLine();
+            if (respuesta == "No" || respuesta == "no")
+            {
+                strClave = true;
+            }
+            else if (respuesta == "Si" || respuesta == "si")
+            {
+                strClave = false;
+            }
+            else 
+            { 
+                Console.WriteLine("Error"); 
+                strClave = false ;
+            }
+            listaNumeros.Add(dblNumero);
+            if (strClave == false) break;
+        }
+        Console.WriteLine("El promedio de tu lista de numeros es:"+Promedio(listaNumeros));
+
+        
+    }
+}
